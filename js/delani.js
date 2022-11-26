f11cabb58a97b63a2cb79b2413fd703cd85c1379
Diana Opiyo <dopiyo@gmail.com>
2022-11-26 22:20:50 +0300

    formatted js file

--- js/delani.js
+++ js/delani.js
@@ -1,41 +1,41 @@
 
-  /*Hovering title on portfolio*/
-  $(document).ready(function(){
-    $(".portfolio-img").hover(function(e){
-      const target = e.target;
-      if (target.matches("img")){
-        const titlename = target.alt.toUpperCase();
-        $(target).prop("title", titlename)
-      }
-    })
-  });
-  /*Toggle effect on the what we do page*/
-  $(document).ready(function() {
-    $("#design-image").click(function(){
-      $("#design").toggle();
-      $(".hide-design-image").toggle();
-    })
-  
-    $("#development-image").click(function(){
-      $("#development").toggle();
-      $(".hide-development-image").toggle();
-    })
-  
-    $("#product-image").click(function(){
-      $("#product").toggle();
-      $(".hide-product-image").toggle();
-    })
-  })
-// Email alert message
-  $('.btn').click(function () {
-    var Name = $('#your-name').val();
-    var Email = $('#email').val();
-    var Message = $('#comment');
-    var key ='https://formspree.io/f/myyvvqqj';
-    if (Name == '' || Email == '' || Message == '') {
-        alert('Please make sure you have filled in the form correctly!');
-    } else {
-        alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+/*Hovering title on portfolio*/
+$(document).ready(function () {
+  $(".portfolio-img").hover(function (e) {
+    const target = e.target;
+    if (target.matches("img")) {
+      const titlename = target.alt.toUpperCase();
+      $(target).prop("title", titlename)
     }
+  })
+});
+/*Toggle effect on the what we do page*/
+$(document).ready(function () {
+  $("#design-image").click(function () {
+    $("#design").toggle();
+    $(".hide-design-image").toggle();
+  })
+
+  $("#development-image").click(function () {
+    $("#development").toggle();
+    $(".hide-development-image").toggle();
+  })
+
+  $("#product-image").click(function () {
+    $("#product").toggle();
+    $(".hide-product-image").toggle();
+  })
+})
+// Email alert message
+$('.btn').click(function () {
+  var Name = $('#your-name').val();
+  var Email = $('#email').val();
+  var Message = $('#comment');
+  var key = 'https://formspree.io/f/myyvvqqj';
+  if (Name == '' || Email == '' || Message == '') {
+    alert('Please make sure you have filled in the form correctly!');
+  } else {
+    alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+  }
 });
 
