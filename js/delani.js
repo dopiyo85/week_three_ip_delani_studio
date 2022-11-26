4c77ef434e53a24074534f79c32f2a5740e94eea
Diana Opiyo <dopiyo@gmail.com>
2022-11-26 21:53:03 +0300

    did some edits

--- js/delani.js
+++ js/delani.js
@@ -8,4 +8,34 @@
         $(target).prop("title", titlename)
       }
     })
+  });
+  
+  $(document).ready(function() {
+    $("#design-image").click(function(){
+      $("#design").toggle();
+      $(".hide-design-image").toggle();
+    })
+  
+    $("#development-image").click(function(){
+      $("#development").toggle();
+      $(".hide-development-image").toggle();
+    })
+  
+    $("#product-image").click(function(){
+      $("#product").toggle();
+      $(".hide-product-image").toggle();
+    })
   })
+
+  $('.btn').click(function () {
+    var Name = $('#your-name').val();
+    var Email = $('#email').val();
+    var Message = $('#comment');
+    var key ='https://formspree.io/f/myyvvqqj';
+    if (Name == '' || Email == '' || Message == '') {
+        alert('Please make sure you have filled in the form correctly!');
+    } else {
+        alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+    }
+});
+
